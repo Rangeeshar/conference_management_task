9427c4a6101ddac6503ceb8dd36179342c080c13
Rangeeshar <rangees28@gmail.com>
2021-11-28 21:27:21 +0530

    initial commit

---
+++ app/models/base.py
@@ -0,0 +1,24 @@
+from constants import db
+from datetime import datetime
+
+class Base(db.Model):
+    __abstract__ = True
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    deleted_at = db.Column(db.DateTime)
+
+    def create(self):
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self, update_data, filter={}):
+        self.updated_at = datetime.now()
+        db.session.query(self.__class__).filter_by(**filter).update(update_data)
+        db.session.commit() 
+
+
+    def fetch(self, filters={}):
+        results = db.session.query(self.__class__).filter_by(**filters).order_by(self.__class__.created_at).all()
+        return [result.native_to_dict() for result in results]
---
+++ factory.py
@@ -0,0 +1,11 @@
+from constants import db, migrate
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'secret string'
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:new_password@localhost/conference'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
+    return app
